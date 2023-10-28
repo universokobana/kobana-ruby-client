503c7a964b4c143807ec1e6a7f24688a0fa65594
FRANCIELLE DE ABREU <francielle@alexandria.solar>
2023-10-28 17:25:00 -0300

    feat initial setup

---
+++ lib/kobana_ruby_client.rb
@@ -0,0 +1,10 @@
+require "kobana_ruby_client/version"
+require "kobana_ruby_client/base"
+require "kobana_ruby_client/charge"
+require "share_api_gem"
+
+
+module KobanaRubyClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
