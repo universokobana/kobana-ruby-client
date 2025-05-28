3154e12bc8a757ca4bd8a1b4295c3d6b9fbf6d9b
Natália Ozawa <118938028+NatiOzawa@users.noreply.github.com>
2025-05-28 17:49:06 -0300

    [VLV-179] feat: :sparkles: add financial/account resource (#4)
    * feat: :sparkles: add financial/account resource and fix bank_billet and pix specs
    
    * refactor: :recycle: apply RuboCop formatting and fixes
    
    * chore: remove token

---
+++ lib/kobana/resources/financial/account.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Kobana
+  module Resources
+    module Financial
+      class Account < Base
+        include ResourceOperations
+
+        @resource_endpoint = "financial/accounts"
+
+        class << self
+          attr_reader :resource_endpoint
+        end
+      end
+    end
+  end
+end
---
+++ spec/factories/financial_account.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :financial_account, class: Hash do
+    uid { "0196f5e5-dd65-7322-9f36-c3979af436b7" }
+    account_number { "00000000000000024618560" }
+    account_digit { "3" }
+    agency_number { "79731" }
+    agency_digit { "3" }
+    financial_provider_slug { "sicoob" }
+    kind { "checking" }
+    person_info { { name: "Margret Santos Filgueiras", document_number: "864.668.358-39" } }
+    bank_id { "206" }
+    custom_name { "Bancoob/Sicoob CC: 81519387-3" }
+
+    initialize_with { attributes }
+  end
+end
