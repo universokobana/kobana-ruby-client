632411e51f581c5c6d02e56961108f5b39dbd66f
Kivanio Barbosa <kivanio@gmail.com>
2025-08-09 10:53:49 -0400

    Refactor String class to check for method definitions before adding pluralize and underscore methods

--- lib/support/string.rb
+++ lib/support/string.rb
@@ -1,21 +1,27 @@
 # frozen_string_literal: true
 
-class String
-  def pluralize
-    if end_with?("y") && !%w[a e i o u].include?(self[-2].downcase)
-      "#{self[0..-2]}ies"
-    elsif end_with?("s", "sh", "ch", "x", "z")
-      "#{self}es"
-    else
-      "#{self}s"
+unless defined?(ActiveSupport)
+  class String
+    unless method_defined?(:pluralize)
+      def pluralize
+        if end_with?("y") && !%w[a e i o u].include?(self[-2].downcase)
+          "#{self[0..-2]}ies"
+        elsif end_with?("s", "sh", "ch", "x", "z")
+          "#{self}es"
+        else
+          "#{self}s"
+        end
+      end
     end
-  end
 
-  def underscore
-    word = gsub("::", "/")
-    word.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-    word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
-    word.tr!("-", "_")
-    word.downcase
+    unless method_defined?(:underscore)
+      def underscore
+        word = gsub("::", "/")
+        word.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+        word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+        word.tr!("-", "_")
+        word.downcase
+      end
+    end
   end
 end
